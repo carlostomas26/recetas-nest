f3e1cbd3a2bcf05e96384a59dedde202ccfda396
carlostomas26 <50860137+carlostomas26@users.noreply.github.com>
2020-02-04 18:47:16 +0100

    Procfile added

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  let puerto = process.env.PORT || 443;
+  await app.listen(puerto);
+  //await app.listen(3000);
 }
 bootstrap();
